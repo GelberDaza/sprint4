016d0d67afdc76cee810c1d8eed2e4916899357e
GelberDaza <gelber.daza.salgado@gmail.com>
2023-12-04 12:47:29 -0500

    cambiosNmbres

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
+st.title ("Proyecto4")        
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-        
+st.header ("histograma")        
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
             st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -21,4 +21,4 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
-    
+st.header("Diagrama de Dispercion")    
