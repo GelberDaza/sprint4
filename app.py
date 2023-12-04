9df15540ebe84ad95890cf66e13230030b36c650
GelberDaza <gelber.daza.salgado@gmail.com>
2023-12-04 14:00:02 -0500

    dispersion

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import streamlit as st
 st.title ("Proyecto4")        
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-st.header ("histograma")        
+st.header ("Histograma")        
 if hist_button: # al hacer clic en el botón
             # escribir un mensaje
             st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
+                    
             # crear un histograma
             fig = px.histogram(car_data, x="odometer")
         
@@ -21,4 +21,18 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
-st.header("Diagrama de Dispercion")    
+st.header("Diagrama de Dispersion")    
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un Diagrama de Dispersion para el conjunto de datos de anuncios de venta de coches por precio')
+                    
+            # crear un histograma
+            fig = px.scatter(car_data, x="odometer", y="price") 
+        
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True) 
+            
+
+            # crear una casilla de verificación
+build_histogram = st.checkbox('Diagrama de Dispersion')
